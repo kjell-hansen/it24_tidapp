f8485736a2768d55fb082b5f41ec2272a2d6d2ce
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2026-04-22 11:28:34 +0300

    Projektplan och databasdiagram har lagts till

---
+++ api/src/Settings_template.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Inställningar för api:et
+ * 
+ */
+class Settings {
+
+    public string  $dsn = "";
+    public string  $dbUser = "";
+    public string  $dbPassword = "";
+}
