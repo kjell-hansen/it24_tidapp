3467ae586aa9161b563de0a0c2671b72fe985d74
ITSTÖD\kjellh <kjell.hansen@gymnasium.ax>
2026-05-18 13:54:13 +0300

    Testcases för hämta alla aktiviteter och hämta enskild aktivitet fungerar

--- api/test/TestActivities.php
+++ api/test/TestActivities.php
@@ -7,7 +7,7 @@ require_once '../src/activities.php';
  * Funktion för att testa alla aktiviteter
  * @return string html-sträng med resultatet av alla tester
  */
-function allaActivityTester(): string {
+function allaActivityTester():string {
     // Kom ihåg att lägga till alla funktioner i filen!
     $retur = "";
     $retur .= test_HamtaAllaAktiviteter();
@@ -21,12 +21,22 @@ function allaActivityTester(): string {
 
 /**
  * Tester för funktionen hämta alla aktiviteter
- * @return string html-sträng med alla resultat för testerna 
+ * @return string html-sträng med alla resultat för testerna
  */
-function test_HamtaAllaAktiviteter(): string {
+function test_HamtaAllaAktiviteter():string {
     $retur = "<h2>test_HamtaAllaAktiviteter</h2>";
     try {
-        $retur .= "<p class='error'>Inga tester implementerade</p>";
+        $svar = hamtaAllaAktiviteter();
+        if ($svar->getStatus() === 200) {
+            $retur .= "<p class='ok'>Hämta alla aktiviteter returnerade 200 som förväntat<p>";
+            if (array_key_exists("activities", $svar->getContent())) {
+                $retur .= "<p class='ok'>" . count($svar->getContent()['activities']) . " poster returnerades</p>";
+            } else {
+                $retur .= "<p class='error'>Arrayen med poster ('activities') saknas</p>";
+            }
+        } else {
+            $retur .= "<p class='error'>Hämta alla aktiviteter returnerade {$svar->getStatus()}, 200 förväntades</p>";
+        }
     } catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
@@ -36,12 +46,52 @@ function test_HamtaAllaAktiviteter(): string {
 
 /**
  * Tester för funktionen hämta enskild aktivitet
- * @return string html-sträng med alla resultat för testerna 
+ * Tester för felaktiga inparametrar (-1 och 'sju') genomförs
+ * Tester för befintliga och saknade id genomförs
+ * @return string html-sträng med alla resultat för testerna
  */
-function test_HamtaEnAktivitet(): string {
+function test_HamtaEnAktivitet():string {
     $retur = "<h2>test_HamtaEnAktivitet</h2>";
     try {
-        $retur .= "<p class='error'>Inga tester implementerade</p>";
+        $svar = hamtaEnskildAktivitet("-1");
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta aktivitet med id=-1 returnerade 400, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta aktivitet med id=-1 returnerade {$svar->getStatus()}, 400 förväntades</p>";
+        }
+
+        $svar = hamtaEnskildAktivitet("sju");
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta aktivitet med id='sju' returnerade 400, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta aktivitet med id='sju' returnerade {$svar->getStatus()}, 400 förväntades</p>";
+        }
+
+        // Testa hämta id som inte finns
+        $alla = hamtaAllaAktiviteter();
+        $poster = $alla->getContent()['activities'];
+        $maxId=0;
+        foreach ($poster as $item) {
+            if($item->id>$maxId) {
+                $maxId=$item->id;
+            }
+        }
+        $svar = hamtaEnskildAktivitet((string) ($maxId+1));
+        if ($svar->getStatus() === 400) {
+            $retur .= "<p class='ok'>Hämta post med id som inte finns (". ($maxId+1) .") returnerade 400, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta post med id som inte finns (". ($maxId+1) .") returnerade {$svar->getStatus()}, 400 förväntades</p>";
+        }
+        // Testa hämta id som finns
+        $id=$poster[0]->id;
+        $svar = hamtaEnskildAktivitet((string) $id);
+        if ($svar->getStatus() === 200) {
+            $retur .= "<p class='ok'>Hämta post med id som finns ($id) returnerade 200, som förväntat</p>";
+        } else {
+            $retur .= "<p class='error'>Hämta post med id som finns ($id) returnerade {$svar->getStatus()}, 200 förväntades</p>";
+        }
+
+
     } catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
     }
@@ -51,9 +101,9 @@ function test_HamtaEnAktivitet(): string {
 
 /**
  * Tester för funktionen spara aktivitet
- * @return string html-sträng med alla resultat för testerna 
+ * @return string html-sträng med alla resultat för testerna
  */
-function test_SparaNyAktivitet(): string {
+function test_SparaNyAktivitet():string {
     $retur = "<h2>test_SparaNyAktivitet</h2>";
 
     try {
@@ -67,9 +117,9 @@ function test_SparaNyAktivitet(): string {
 
 /**
  * Tester för uppdatera aktivitet
- * @return string html-sträng med alla resultat för testerna 
+ * @return string html-sträng med alla resultat för testerna
  */
-function test_UppdateraAktivitet(): string {
+function test_UppdateraAktivitet():string {
     $retur = "<h2>test_UppdateraAktivitet</h2>";
 
     try {
@@ -83,9 +133,9 @@ function test_UppdateraAktivitet(): string {
 
 /**
  * Tester för funktionen radera aktivitet
- * @return string html-sträng med alla resultat för testerna 
+ * @return string html-sträng med alla resultat för testerna
  */
-function test_RaderaAktivitet(): string {
+function test_RaderaAktivitet():string {
     $retur = "<h2>test_RaderaAktivitet</h2>";
     try {
         $retur .= "<p class='error'>Inga tester implementerade</p>";
